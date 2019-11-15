c5c916d0c17e058a59addf84813289975211b71a
Jacob Brookins <wjbrookins@crimson.ua.edu>
2019-11-14 21:48:19 -0600

    Moving constants to this so they are whole app

---
+++ app/src/main/java/com/example/musicqueue/Constants.java
@@ -0,0 +1,12 @@
+package com.example.musicqueue;
+
+public class Constants {
+
+    //Colors
+    public static final String UNCHECKED_COLOR = "#192125";
+    public static final String CHECKED_COLOR = "#F5F5F5";
+
+    //Firebase References
+    public static final String FIRESTORE_QUEUE_COLLECTION = "queues";
+    public static final String FIRESTORE_SONG_COLLECTION = "songs";
+}
