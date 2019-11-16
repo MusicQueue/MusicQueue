41284b50cbdbacae658c805c28149beeec1a2644
dplong1993 <43188659+dplong1993@users.noreply.github.com>
2019-11-16 17:45:01 -0600

    Added AbstractSong and SongModel
    Created the AbstactSong class and the SongModel class which implements the AbstractSong

---
+++ app/src/main/java/com/example/musicqueue/models/AbstractSong.java
@@ -0,0 +1,26 @@
+package com.example.musicqueue.models;
+
+import androidx.annotation.Nullable;
+
+public abstract class AbstractSong {
+
+    @Nullable
+    public abstract String getSongName();
+
+    public abstract void setSongName(@Nullable String songName);
+
+    @Nullable
+    public abstract String getArtistName();
+
+    public abstract void setArtistName(@Nullable String artistName);
+
+    @Nullable
+    public abstract int getSongRank();
+
+    public abstract void setSongRank(@Nullable int songRank);
+
+    @Nullable
+    public abstract String getDocId();
+
+    public abstract void setDocId(@Nullable String docid);
+}
---
+++ app/src/main/java/com/example/musicqueue/models/SongModel.java
@@ -0,0 +1,52 @@
+package com.example.musicqueue.models;
+
+import androidx.annotation.Nullable;
+
+import com.google.firebase.firestore.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class SongModel extends AbstractSong {
+
+    private String songName;
+    private String artistName;
+    private int songRank;
+    private String docid;
+
+    public SongModel() {}
+
+    public SongModel(@Nullable String songName, @Nullable String artistName,
+                     @Nullable int songRank, @Nullable String docid) {
+        this.songName = songName;
+        this.artistName = artistName;
+        this.songRank = songRank;
+        this.docid = docid;
+    }
+
+    @Override
+    public void setSongName(String s) { this.songName = s; }
+
+    @Override
+    public void setArtistName(String s) { this.artistName = s; }
+
+    @Override
+    public void setSongRank(int i) { this.songRank = i; }
+
+    @Override
+    public void setDocId(String s) { this.docid = s; }
+
+    @Override
+    @Nullable
+    public String getSongName() { return this.songName; }
+
+    @Override
+    @Nullable
+    public String getArtistName() { return this.artistName; }
+
+    @Override
+    @Nullable
+    public int getSongRank() { return this.songRank; }
+
+    @Override
+    @Nullable
+    public String getDocId() { return this.docid; }
+}
